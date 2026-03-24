cd5854a2db4af4edb28e19798cf56345d1418c58
Marisel <herokubilling1@gmail.com>
2026-03-24 20:51:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     PORT: process.env.PORT || 50900,
     SESSION_PREFIX: process.env.SESSION_PREFIX || "Mercedes~",
     GC_JID: process.env.GC_JID || "DxgUM6jXbj98t2TgtseHNM",
-    DATABASE_URL: process.env.DATABASE_URL || "mongodb+srv://ellyongiro8:QwXDXE6tyrGpUTNb@cluster0.tyxcmm9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // Your Db URL here(optional). Can either be mongodb or postreSQL
+    DATABASE_URL: process.env.DATABASE_URL || "furii3L3ijdpwYB-vZ_jej7CxvNjFESk", // Your Db URL here(optional). Can either be mongodb or postreSQL
     BOT_REPO: process.env.BOT_REPO || "https://github.com/betingrich4/Mercedes",
     WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029Vajvy2kEwEjwAKP4SI0x",
     MSG_FOOTER: process.env.MSG_FOOTER || "> *Marisel*",
